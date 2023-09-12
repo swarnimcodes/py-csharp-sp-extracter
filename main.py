fdc489eb40c6394dc6a9f6cff294da54ab7eb78a
Swarnim B - MasterSoft <swarnim14.9@hotmail.com>
2023-09-12 12:25:05 +0530

    added line numbers

--- main.py
+++ main.py
@@ -41,9 +41,11 @@ def main() -> None:
     # Headers
     ws["A1"] = "File Path"
     ws["B1"] = "SP Count"
-    ws["C1"] = "SP List"
-    ws["D1"] = "Table Count"
-    ws["E1"] = "Table List"
+    ws['C1'] = "SP Line No."
+    ws["D1"] = "SP List"
+    ws["E1"] = "Table Count"
+    ws["F1"] = "Table Line No."
+    ws["G1"] = "Table List"
     # r=root, d=directories, f = files
     for r, d, f in os.walk(folder):
         for file in f:
@@ -55,14 +57,19 @@ def main() -> None:
         sp_count = 0
         table_count = 0
         sp_list = []
+        sp_ln = []
         table_list = []
+        tbl_ln = []
         print(file)
         with open(file, "r") as f:
             lines = f.readlines()
+            line_num = 0
             for line in lines:
+                line_num += 1
                 if not line.startswith("//"):
                     if bool([ele for ele in sp_func if (ele in line)]):
                         sp_list.extend(re.findall(r'"([^"]*)"', line))
+                        sp_ln.append(line_num)
                         sp_count = sp_count + 1
                     elif bool([ele for ele in tbl_func if (ele in line)]):
                         match = re.search(r'"([^"]*)"', line)
@@ -70,11 +77,13 @@ def main() -> None:
                             m_tbl = match.group(1)
                             if not bool(re.search(r"\s", m_tbl)):
                                 table_list.append(m_tbl)
+                                tbl_ln.append(line_num)
                                 table_count += 1
                             elif bool(re.search(r"\s", m_tbl)):
                                 # tokenize should just return a list of tbls
                                 tbl_list = tokenize(m_tbl)
                                 table_list.extend(tbl_list)
+                                tbl_ln.append(line_num)
                                 table_count += 1
         print(
             f"Filename:\t{file}\nSP Count:\t{sp_count}\nSP List:\t{sp_list}\n"
@@ -82,8 +91,10 @@ def main() -> None:
         )
         xl_append.append(file)
         xl_append.append(sp_count)
+        xl_append.append("\n".join(map(str, sp_ln)))
         xl_append.append("\n".join(sp_list))
         xl_append.append(table_count)
+        xl_append.append("\n".join(map(str, tbl_ln)))
         xl_append.append("\n".join(table_list))
         ws.append(xl_append)
 
